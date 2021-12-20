1398986c001a0a0d4cf2296e4c20c8acd9df41ef
carlospuigserver <cpuiggut@myuax.com>
2021-12-20 21:56:44 +0100

    Bucle figuras

--- Estrella.py
+++ Estrella.py
@@ -3,5 +3,18 @@ import turtle
 # Defino función estrella
 def estrella(n):
     def angulo():
-        angulo=0
-        if
+        angulo=0        # Defino condiciones para las puntas de las estrellas
+        if n%3==0:
+            angulo=(360/n)+120
+        elif n%4 ==0:
+            angulo=180-(360/n)
+        else:
+            return angulo
+    turtle.speed(0)
+    for _ in range(n):
+        turtle.right(angulo()) # Bucle para crear la figura
+        turtle.forward(200)
+    turtle.done()
+
+
+
